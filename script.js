5e7d26fba2d8015dcaf122f68d99ac7a72281e6d
JoeDevwWeb <101819884+JoeDevwWeb@users.noreply.github.com>
2022-06-08 13:39:02 -0300

    versao0/1

---
+++ script.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAGqIkpR4nVkdoxgsCDpxYFutEJ0O_mOY",
+    authDomain: "diariodoaluno-d32c8.firebaseapp.com",
+    projectId: "diariodoaluno-d32c8",
+    storageBucket: "diariodoaluno-d32c8.appspot.com",
+    messagingSenderId: "843419785955",
+    appId: "1:843419785955:web:2fb0903f48c83661a5c3d6"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const storage = firebase.storage();
